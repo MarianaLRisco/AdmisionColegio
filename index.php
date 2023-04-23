178eccd339de0ac56fa526614eb18f3427becb71
Mariana Risco Cosavalente <83486401+MarianaLRisco@users.noreply.github.com>
2023-04-23 18:49:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/png" href="imagenes/logo1.png">
+    <link rel="icon" type="image/png" href="">
     <!--BDP-->
     <script src="libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js" charset="utf-8"></script>
     <link rel= "stylesheet" href="libs/bootstrap-datepicker/css/bootstrap-datepicker.css">
